b4fef7d0a0ae4a9fbcadf57bf23a2eff4b06604e
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-05 21:46:23 +0530

    Added Axios for data fetching

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import axios from "axios";
 import { useEffect, useState } from "react"
 
 
@@ -6,9 +7,8 @@ function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    fetch('https://localhost:5001/api/activities')
-      .then(response => response.json())
-      .then(data => setActivities(data))
+    axios.get<Activity[]>('https://localhost:5001/api/activities')
+      .then(response => setActivities(response.data))
 
       return () => {}
   }, [])
