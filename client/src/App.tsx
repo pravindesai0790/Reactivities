6184c21728125402cedd9d6a4ade1fd781749e8f
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-05 21:12:23 +0530

    Created an Activity type in TypeScript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     fetch('https://localhost:5001/api/activities')
