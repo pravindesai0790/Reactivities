8e362092cb6c6cae74b53fd301f84081dff74830
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-05 19:12:45 +0530

    Added CORS configuration in API and fetchingand displaying data from the API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react"
 
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/activities')
+      .then(response => response.json())
+      .then(data => setActivities(data))
+
+      return () => {}
+  }, [])
 
   return (
-    <h3>Reactivities</h3>
+    <div>
+      <h3 style={{ color: 'red' }}>Reactivities</h3>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
+    
   )
 }
 
