da95c3f80520332f1d3b25d78812a98850686e65
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-06 18:25:58 +0530

    Added a Mediator handler for Creating and Editing an activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using Application.Activities.Commands;
 using Application.Activities.Queries;
 using Domain;
 using MediatR;
@@ -17,6 +18,20 @@ namespace API.Controllers
         public async Task<ActionResult<Activity>> GetActivity(string id)
         {
             return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateActivity(Activity activity)
+        {
+            return await Mediator.Send(new CreateActivity.Command{ Activity = activity });
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> EditActivity(Activity activity)
+        {
+            await Mediator.Send(new EditActivity.Command{ Activity = activity });
+            
+            return NoContent();
         }
     }
 }
