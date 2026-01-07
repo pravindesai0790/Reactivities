0cdb5e0045866928a49c6635282d9b061656954c
pravindesai0790 <pravindesai0790@gmail.com>
2026-01-07 11:59:27 +0530

    Added Navbar component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,31 +1,40 @@
-import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import {
+  Container,
+  CssBaseline,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 import axios from "axios";
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
+import NavBar from "./NavBar";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get<Activity[]>('https://localhost:5001/api/activities')
-      .then(response => setActivities(response.data))
+    axios
+      .get<Activity[]>("https://localhost:5001/api/activities")
+      .then((response) => setActivities(response.data));
 
-      return () => {}
-  }, [])
+    return () => {};
+  }, []);
 
   return (
     <>
-      <Typography variant='h3'>Reactivities</Typography>
-      <List>
-        {activities.map((activity) => (
-          <ListItem key={activity.id}>
-            <ListItemText>{activity.title}</ListItemText>
-          </ListItem>
-        ))}
-      </List>
+      <CssBaseline />
+      <NavBar />
+      <Container maxWidth="xl" sx={{mt: 3}}>
+        <List>
+          {activities.map((activity) => (
+            <ListItem key={activity.id}>
+              <ListItemText>{activity.title}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </Container>
     </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
